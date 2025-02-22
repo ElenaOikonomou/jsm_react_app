7d4568eb4c498e57f84cb8782309606e23dd05ab
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2025-02-21 18:08:02 -0800

    start_my_netflix_clone_app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
-import  { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import "./App.css";
+import React from 'react'
 
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0)
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(()=> {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked])
-
-  
+const App = () => {
   return (
-    <div className="card" onClick={()=> setCount((prevState) => prevState + 1)}>
-      <h2>{title} <br/> {count? count : null}</h2> {/* conditional rendering*/}
-      
-      
-      <button onClick= {()=>setHasLiked((prevState) => !prevState)}>
-        {hasLiked ? "Liked" : "Like"}
-      </button>
-    </div>
-  );
-};
+    <main>
+      <div className="pattern"></div>
 
-
-Card.propTypes = {
-  title: PropTypes.string.isRequired,
-};
-
-export default function App() {
-  return (
-    <div className="card-container">
-      <Card title="Lion King" />
-      <Card title="Avatar" />
-      <Card title="Snow White" />
-    </div>
-  );
+      <div className="wrapper">
+        <header>
+          <h1>Find Movies You will Enjoy Without The Hassle</h1>
+        </header>
+      </div>
+  </main>
+  )
 }
+
+export default App
