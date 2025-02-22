9ab8b79d8f39dddcf53036bb7504fb5891fa1515
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2025-02-21 17:09:19 -0800

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import  { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./App.css";
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0)
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(()=> {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked])
+
+  
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={()=> setCount((prevState) => prevState + 1)}>
+      <h2>{title} <br/> {count? count : null}</h2> {/* conditional rendering*/}
+      
+      
+      <button onClick= {()=>setHasLiked((prevState) => !prevState)}>
+        {hasLiked ? "Liked" : "Like"}
+      </button>
     </div>
   );
 };
 
- 
+
 Card.propTypes = {
   title: PropTypes.string.isRequired,
 };
@@ -18,7 +31,7 @@ Card.propTypes = {
 export default function App() {
   return (
     <div className="card-container">
-      <Card title="Lion King" rating={5} isCool={true}/>
+      <Card title="Lion King" />
       <Card title="Avatar" />
       <Card title="Snow White" />
     </div>
