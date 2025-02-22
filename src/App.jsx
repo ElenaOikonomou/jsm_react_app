07b470e132f00928908c37f6a241caab7c0ce670
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2025-02-21 16:13:48 -0800

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from "react"
-import "./App.css"
+import React from "react";
+import PropTypes from "prop-types";
+import "./App.css";
 
-function Card({title]){
+const Card = ({ title }) => {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="card">
+      <h2>{title}</h2>
     </div>
-  )
-}
+  );
+};
+
+ 
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default function App() {
-return(
-  <div>
-    <Card title="Lion King"/>
-  
-    <Card title="Cinderella"/>
- 
-    <Card title="Snow White"/>
-  </div>
-  
-  
-)
+  return (
+    <div className="card-container">
+      <Card title="Lion King" rating={5} isCool={true}/>
+      <Card title="Avatar" />
+      <Card title="Snow White" />
+    </div>
+  );
 }
